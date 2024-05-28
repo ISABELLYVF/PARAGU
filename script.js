b2be1926f70f5bd473c5326ea2662c50718264d3
ISABELLYVF <126249501+ISABELLYVF@users.noreply.github.com>
2024-05-28 09:35:28 -0300

    Update script.js

--- script.js
+++ script.js
@@ -8,74 +8,54 @@ const perguntas = [
     {
         enunciado: "Assim que saiu da escola você se depara com uma menina e ela ta dando em cima de vc, oque vc faz?",
         alternativas: [
-            "sair correndo e fingir ser um deb mental!",
-            "conversar com ela!"
+            "sair correndo e fingir ser um deb mental",
+            "conversar com ela"
         ]
     },
     {
         enunciado: "depois a isabelly te convida pra sair, que atitude você toma?",
         alternativas: [
             "aceitar o convite!",
-            "dizer que esta ocupado e sair com a menina estranha",
+            "dizer que esta ocupado e sair com a menina estranha"
         ]
     },
     {
-        enunciado: " você esta em um campeonato de vôlei, e no final da partida as meninas pedem um autografo no peito delas, oque você faria?",
+        enunciado: " você esta em um campeonato de vôlei, e no final da partida as meninas pedem um autografo no peito delas, oque você faria",
         alternativas: [
-            "assina no peito de todas :(",
-            "ou esfrega a aliança na cara delas"
+            "Alternativa assina no peito de todas ",
+            "esfrega a aliança na cara delas"
         ]
     },
     {
         enunciado: "você ve o cara dando em cima da isa e dando flores, oque você faria?",
         alternativas: [
-            "deixaria a isa com o cara por que ele é melhor que você.",
+            "deixaria a isa com o cara por que ele é melhor que você",
             "ou a puxaria pela cintura e daria um beijão "
         ]
     },
     {
         enunciado: "oque você faria no dia do casamento da isa de não fosse com você",
         alternativas: [
-           "rouba a arma do seu pai e atira emtodo mundo",
-            "deixar se casar e sofra pelo restoda sua vida "
+            "rouba a arma do seu pai e atira emtodo mundo",
+            "deixar se casar e sofra pelo restoda sua vida"
         ]
     },
-];
+]; 
 
 let atual = 0;
 let perguntaAtual;
-
 function mostraPergunta() {
-    if (atual >= perguntas.length) {
-        mostraResultado();
-        return;
-    }
     perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
-    caixaAlternativas.textContent = "";
-    mostraAlternativas();
+    mostraalternativa()
 }
-
-function mostraAlternativas(){
-    for(const alternativa of perguntaAtual.alternativas) {
-        const botaoAlternativas = document.createElement("button");
-        botaoAlternativas.textContent = alternativa.texto;
-        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
+function mostraalternativa() {
+    for(const alternativa of perguntaAtual.alternativas){
+        const botaoAlternativas = document.createElement("button"); 
+        botaoAlternativas.textContent = alternativa;
         caixaAlternativas.appendChild(botaoAlternativas);
     }
 }
 
-function respostaSelecionada(opcaoSelecionada) {
-    const afirmacoes = opcaoSelecionada.afirmacao;
-    historiaFinal += afirmacoes + " ";
-    atual++;
-    mostraPergunta();
-}
-
-function mostraResultado() {
-    caixaPerguntas.textContent = "Em 2049...";
-    textoResultado.textContent = historiaFinal;
-    caixaAlternativas.textContent = "";
-}
-
 mostraPergunta();
+
