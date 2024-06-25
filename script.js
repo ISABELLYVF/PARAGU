982306fb3f545d63d2abcecf1358675453af5832
ISABELLYVF <126249501+ISABELLYVF@users.noreply.github.com>
2024-06-25 10:14:06 -0300

    Update script.js

--- script.js
+++ script.js
@@ -9,12 +9,12 @@ const perguntas = [
         enunciado: "Assim que saiu da escola você se depara com uma menina e ela ta dando em cima de vc, oque vc faz?",
         alternativas: [
             {
-                texto:"sair correndo e fingir ser um deb mental";
-                afirmacao: "afirmacao";
+                texto:"sair correndo e fingir ser um deb mental",
+                afirmacao: "afirmacao",
             }
             {
-                texto:"conversar com ela";
-                afirmacao: "afirmacao";
+                texto:"conversar com ela",
+                afirmacao: "afirmacao",
             }
 ]
     },
@@ -22,12 +22,12 @@ const perguntas = [
         enunciado: "depois a isabelly te convida pra sair, que atitude você toma?",
         alternativas: [
             {
-                texto:" aceitar o convite" ;
-                    afirmacao: "afirmacao";
+                texto:" aceitar o convite",
+                    afirmacao: "afirmacao",
     }
     {
-              texto: " dizer que esta ocupado e sair com a menina estranha";
-              afirmacao: "afirmacao";
+              texto: " dizer que esta ocupado e sair com a menina estranha",
+              afirmacao: "afirmacao",
     }
         ]
     },
@@ -35,36 +35,36 @@ const perguntas = [
         enunciado: " você esta em um campeonato de vôlei, e no final da partida as meninas pedem um autografo no peito delas, oque você faria",
         alternativas: [
             {
-            texto: "assinar no peito de todas";
-            afirmacao:"afirmacao";
+            texto: "assinar no peito de todas",
+            afirmacao:"afirmacao",
         }
         {
-         texto: "esfregar a alianca na cara delas";
-           afirmacao: "afirmacao";
+         texto: "esfregar a alianca na cara delas",
+           afirmacao: "afirmacao",
         ]
         },
     {
         enunciado: "você ve o cara dando em cima da isa e dando flores, oque você faria?",
         alternativas: [
             {
-            texto: "deixaria a Isa com o cara por que ele é melhor que você";
-            afirmacao: "afirmacao";
+            texto: "deixaria a Isa com o cara por que ele é melhor que você",
+            afirmacao: "afirmacao",
     }
     {
-            texto: "puxaria a Isa pra longe dele";
-            afirmacao:" afirmacao";
+            texto: "puxaria a Isa pra longe dele",
+            afirmacao:" afirmacao",
         ]
     },
     {
         enunciado: "oque você faria no dia do casamento da isa de não fosse com você",
         alternativas: [
             {
-                texto:" roubar a arma do seu pai e atirar em todo mundo";
-                afirmacao:"afirmacao";
+                texto:" roubar a arma do seu pai e atirar em todo mundo",
+                afirmacao:"afirmacao",
     }
     {
-               texto:"deixa ela se casar e sofrer pelo resto da vida";
-                   afirmacao:"afirmacao";
+               texto:"deixa ela se casar e sofrer pelo resto da vida",
+                   afirmacao:"afirmacao",
         ]
     },
 ]; 
@@ -85,4 +85,3 @@ function mostraalternativa() {
 }
 
 mostraPergunta();
-
